5c3d29cb7eb409d7c343521b6dbd376e4b8c7ab0
alladeson <alladeson91@gmail.com>
2022-09-02 08:32:40 +0100

    maj variable d'environnrement api_base_url

--- .env.local.php
+++ .env.local.php
@@ -5,5 +5,5 @@
 return array (
   'APP_ENV' => 'dev',
   'APP_SECRET' => 'de4be7a1990b1f5c9fefcb6ef20967a2',
-  'API_BASE_URL' => 'https://localhost:8443/',
+  'API_BASE_URL' => 'https://localhost:8445/',
 );
